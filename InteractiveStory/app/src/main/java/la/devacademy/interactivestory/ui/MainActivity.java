16a8a3acff2a7421d3b8981567f4ce91a2176735
brodriguezaybar <brodriguezaybar@gmail.com>
2016-01-14 16:16:11 -0500

    Improve activities flow

--- InteractiveStory/app/src/main/java/la/devacademy/interactivestory/ui/MainActivity.java
+++ InteractiveStory/app/src/main/java/la/devacademy/interactivestory/ui/MainActivity.java
@@ -37,4 +37,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mNameField.setText("");
+    }
 }
--- InteractiveStory/app/src/main/java/la/devacademy/interactivestory/ui/StoryActivity.java
+++ InteractiveStory/app/src/main/java/la/devacademy/interactivestory/ui/StoryActivity.java
@@ -1,19 +1,32 @@
 package la.devacademy.interactivestory.ui;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import la.devacademy.interactivestory.R;
 import la.devacademy.interactivestory.model.Page;
+import la.devacademy.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
 
-    private Page[] mPages;
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
+    private Page mCurrentPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +34,65 @@ public class StoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        mName = intent.getStringExtra(getString(R.string.key_name));
 
-        if (name == null) {
-            name = "Amigo";
+        if (mName == null) {
+            mName = "Amigo";
         }
 
-        mPages = new Page[7];
+        mImageView = (ImageView) findViewById(R.id.storyImageView);
+        mTextView = (TextView) findViewById(R.id.storyTextView);
+        mChoice1 = (Button) findViewById(R.id.choiceButton1);
+        mChoice2 = (Button) findViewById(R.id.choiceButton2);
+
+        loadPage(0);
+    }
+
+    private void loadPage(int choice) {
+        mCurrentPage = mStory.getPage(choice);
+
+        Drawable drawable = getResources().getDrawable(mCurrentPage.getImageId());
+        mImageView.setImageDrawable(drawable);
+
+        String pageText = mCurrentPage.getText();
+        // Add the name if placeholder included
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+
+        if (mCurrentPage.getIsFInal()) {
+
+            mChoice1.setVisibility(View.INVISIBLE);
+            mChoice2.setText("Jugar otra vez");
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+
+
+        } else {
+
+            mChoice1.setText(mCurrentPage.getChoice1().getText());
+            mChoice2.setText(mCurrentPage.getChoice2().getText());
+
+            mChoice1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int nextPage = mCurrentPage.getChoice1().getNextPage();
+                    loadPage(nextPage);
+                }
+            });
+
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int nextPage = mCurrentPage.getChoice2().getNextPage();
+                    loadPage(nextPage);
+                }
+            });
+
+        }
     }
 
 }
