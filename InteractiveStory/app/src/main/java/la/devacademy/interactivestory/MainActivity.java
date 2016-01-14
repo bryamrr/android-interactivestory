ff3c580a064f66b0944b710b3706800934bcb9b9
brodriguezaybar <brodriguezaybar@gmail.com>
2016-01-13 23:01:46 -0500

    Edit main activity

---
+++ InteractiveStory/app/src/main/java/la/devacademy/interactivestory/MainActivity.java
@@ -0,0 +1,35 @@
+package la.devacademy.interactivestory;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mNameField;
+    private Button mStartButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNameField = (EditText) findViewById(R.id.nameEditText);
+        mStartButton = (Button) findViewById(R.id.startButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameField.getText().toString();
+            }
+        });
+    }
+
+}
